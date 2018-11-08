c68b1d531a1ed92a830008f1fdbe28c30231d7e0
Tom <tomacunningham@gmail.com>
2018-11-07 21:54:02 -0600

    Base file
    The very basic of running an LED light strand, add in the wifi connection from here.

---
+++ BasicLEDLights.ino
@@ -0,0 +1,24 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN D3
+#define LED_STRING_LENGTH 8
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(8, PIN);
+
+void setup() 
+{
+  pixels.begin();
+}
+
+void loop() 
+{
+  //Loop through every LED
+  for(int n = 0; n <= LED_STRING_LENGTH ; n++)
+  {
+    pixels.setPixelColor(n,pixels.Color(0,0,255));
+  }
+  //Call the show pixel function
+  pixels.show();
+  
+  //Add this in here to service the watchdog
+  delay(10);
+}
